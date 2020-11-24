6aebbbc822cdb02a129aadd10d39701432dca465
yusup <ym3594216@gmail.com>
2020-11-24 22:52:49 +0700

    create login page

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,104 @@
 <template>
   <v-app>
-    <v-app-bar app dark color="blue">
-      <v-toolbar-title>Vue Login</v-toolbar-title>
+    <v-app-bar app height="30" color="#808080">
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
-      <v-btn text rounded>About</v-btn>
-      <v-btn text rounded>Contact us</v-btn>
+      <span style="color: white">Call Us Now: +62 812-1133-5608</span>
+      <span class="subtitle-1 pl-6" style="color: white">English</span>
+      <v-menu
+        bottom
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            icon
+          >
+            <v-icon color="white">mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>Indonesia</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
-      <v-card width="500" class="mx-auto mt-9">
-        <v-card-title>Long Area</v-card-title>
-        <v-card-text>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle"/>
-          <v-text-field 
-          label="Password" 
-          :type="showPassword ? 'text' : 'password'"
-          prepend-icon="mdi-lock"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="showPassword = !showPassword"/>
-        </v-card-text>
-
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card width="500" height="600" class="mx-auto mt-9">
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="6" style="padding-top: 50px">
+                  <v-img src="https://via.placeholder.com/250x100" width="250" height="100"></v-img>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8" class="text-center">
+                  Your one stop platform to manage all of your field service management
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8" class="pb-0">
+                  <v-text-field v-model="organizationName" label="Enter your organization's name" solo></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row v-if="error" justify="center">
+                <v-col cols="12" sm="6" md="8" class="pb-0 pt-0">
+                  <span class="red--text">The organization name field cannot empty</span>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8" class="pt-0">
+                  <v-btn @click="submit()" block color="blue">
+                    <span style="color: white">Login</span>
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8" class="text-center">
+                  Not registered yet? <a href="#" style="text-decoration: none">Register here</a>
+                </v-col>
+              </v-row>
+              <v-row justify="left">
+                <v-col cols="12" sm="6" md="8" class="text-center">
+                  <a href="#" style="text-decoration: none">
+                    <v-icon small>mdi-arrow-left</v-icon>
+                    Back to Mile</a>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
 
     <template>
-  <v-footer
-    dark
-    padless
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="teal">
-       
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4"
-          dark
-          icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-title>
-
-      <v-card-text class="py-2 white--text text-center">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-card-text>
-    </v-card>
+  <v-footer>
   </v-footer>
 </template>
   </v-app>
 </template>
 <script>
 export default {
-  data()
-  {
+  data() {
     return{
-      showPassword:false
+      showPassword:false,
+      organizationName: '',
+      error: false,
+    }
+  },
+  methods: {
+    submit: function() {
+      if (this.organizationName === '') this.error = true;
+      else console.log(this.organizationName);
+    }
+  },
+  computed: {
+    validator: function () {
+      return this.organizationName.length > 0
     }
   }
 }
